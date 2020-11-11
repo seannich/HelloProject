eeec3539729bc4effcbec751921cdc26b47cf00d
seannich <seannts9@gmail.com>
2020-11-11 14:54:37 +0800

    Create readme.py

---
+++ readme.py
@@ -0,0 +1,10 @@
+// DAY, DATE - Wednesday, 11 November 2020
+// TEAM NAME, LIST - Team 3, Adeline, Gerald, Naomi, Sean
+
+// ROLES:
+// - ADEL   : QA
+// - GERALD : DEVELOPER 1
+// - NAOMI  : DEVELOPER 2
+// - SEAN   : PROJECT MANAGER
+
+// SLOGAN - "THE BEST PAIN A (WO)MAN CAN GET"
